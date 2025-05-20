ac707c823df350ae319ef0538a537dc3d6b1298f
kiiikuo <kiiikuo2002@gmail.com>
2025-05-20 23:08:45 +0800

    Update Predict.py

--- code/Predict.py
+++ code/Predict.py
@@ -4,10 +4,8 @@ import torch
 
 
 def predict_with_uncertainty(model, region_inputs, mc_samples=100, device='cpu'):
-    """
-    对输入进行 MC Dropout 推理，仅使用区域图作为输入，返回均值和方差
-    """
-    model.eval()  # 设置为评估模式（BN/Dropout）
+
+    model.eval()
     model.to(device)
 
     region_inputs = region_inputs.to(device)
@@ -17,11 +15,11 @@ def predict_with_uncertainty(model, region_inputs, mc_samples=100, device='cpu')
 
     with torch.no_grad():
         for _ in range(mc_samples):
-            model.train()  # 启用 Dropout
+            model.train()
             out = model(region_inputs)
             preds.append(out.unsqueeze(0))
 
-    preds_stack = torch.cat(preds, dim=0)  # (mc_samples, batch, H, W)
+    preds_stack = torch.cat(preds, dim=0)
     mean = preds_stack.mean(dim=0)
     var = preds_stack.var(dim=0)
 
