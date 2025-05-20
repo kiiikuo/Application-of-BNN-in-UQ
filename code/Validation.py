8f067dc501e49c11f9f3a372c77a450759fc4196
kiiikuo <kiiikuo2002@gmail.com>
2025-05-20 23:09:33 +0800

    Update Validation.py

--- code/Validation.py
+++ code/Validation.py
@@ -5,15 +5,7 @@ import torch.nn.functional as F
 
 
 def evaluate_model(model, region_val, Y_val, mc_samples=1, return_std=False):
-    """
-    对验证集进行评估，仅使用区域图作为输入，支持 MC Dropout（多个前向传播）
 
-    返回：
-    - mse: float
-    - mae: float
-    - preds_mean: (N, 320, 416)
-    - [可选] preds_std: (N, 320, 416)
-    """
     model.eval()
     region_val = region_val.to(torch.float32)
     Y_val = Y_val.to(torch.float32)
@@ -23,7 +15,7 @@ def evaluate_model(model, region_val, Y_val, mc_samples=1, return_std=False):
     with torch.no_grad():
         for _ in range(mc_samples):
             if mc_samples > 1:
-                model.train()  # 启用 Dropout
+                model.train()
             else:
                 model.eval()
             preds = model(region_val)
