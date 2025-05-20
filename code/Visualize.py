c3161fbdb931b58a9208467d8e4e535348f951d4
kiiikuo <kiiikuo2002@gmail.com>
2025-05-20 23:09:54 +0800

    Update Visualize.py

--- code/Visualize.py
+++ code/Visualize.py
@@ -5,9 +5,7 @@ import numpy as np
 
 
 def visualize_prediction(mean_pred, var_pred, ground_truth, idx=0):
-    """
-    显示某个样本的预测均值、方差、误差图，并添加参考颜色条
-    """
+    
     mean_np = mean_pred[idx].cpu().numpy()
     var_np = var_pred[idx].cpu().numpy()
     gt_np = ground_truth[idx].cpu().numpy()
@@ -20,21 +18,21 @@ def visualize_prediction(mean_pred, var_pred, ground_truth, idx=0):
     axs[0].set_title("Predicted Mean")
     axs[0].set_xlabel('Longitude')
     axs[0].set_ylabel('Latitude')
-    fig.colorbar(im0, ax=axs[0], orientation='vertical')  # 添加颜色条
+    fig.colorbar(im0, ax=axs[0], orientation='vertical')
 
     # Predictive Variance
     im1 = axs[1].imshow(var_np, cmap='viridis', origin='lower')
     axs[1].set_title("Predictive Variance")
     axs[1].set_xlabel('Longitude')
     axs[1].set_ylabel('Latitude')
-    fig.colorbar(im1, ax=axs[1], orientation='vertical')  # 添加颜色条
+    fig.colorbar(im1, ax=axs[1], orientation='vertical')
 
     # Absolute Error
     im2 = axs[2].imshow(error_np, cmap='hot', origin='lower')
     axs[2].set_title("Absolute Error")
     axs[2].set_xlabel('Longitude')
     axs[2].set_ylabel('Latitude')
-    fig.colorbar(im2, ax=axs[2], orientation='vertical')  # 添加颜色条
+    fig.colorbar(im2, ax=axs[2], orientation='vertical')
 
     plt.tight_layout()
     plt.show()
